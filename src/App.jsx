17d5a380f2dcbe563be952150664517fa3fd83ce
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-04-18 21:07:37 +0300

    navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <button type="button" classNameNameName="btn btn-primary">
-        Primary
-      </button>
-      <button type="button" classNameNameName="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" classNameNameName="btn btn-success">
-        Success
-      </button>
-      <button type="button" classNameNameName="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" classNameNameName="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" classNameNameName="btn btn-info">
-        Info
-      </button>
-      <button type="button" classNameNameName="btn btn-light">
-        Light
-      </button>
-      <button type="button" classNameNameName="btn btn-dark">
-        Dark
-      </button>
-
-      <button type="button" classNameNameName="btn btn-link">
-        Link
-      </button>
-      <div classNameName="dropdown">
-        <button
-          classNameName="btn btn-secondary dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Dropdown button
-        </button>
-        <ul classNameName="dropdown-menu">
-          <li>
-            <a classNameName="dropdown-item" href="#">
-              Action
-            </a>
-          </li>
-          <li>
-            <a classNameName="dropdown-item" href="#">
-              Another action
-            </a>
-          </li>
-          <li>
-            <a classNameName="dropdown-item" href="#">
-              Something else here
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
+      <Navbar />
     </div>
   )
 }
