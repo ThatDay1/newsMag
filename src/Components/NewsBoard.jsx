f97d84492eff0f04df1f57426e969a5dddfee0f8
ThatDay1 <120332992+ThatDay1@users.noreply.github.com>
2024-04-19 16:02:23 +0300

    newsItem section

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem'
 
 const NewsBoard = () => {
   const [articles, setArticles] = useState([])
@@ -7,9 +8,9 @@ const NewsBoard = () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
       import.meta.env.VITE_API_KEY
     }`
-    fetch(url).then((response) =>
-      response.json((data) => setArticles(data.articles))
-    )
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles))
   }, [])
 
   return (
@@ -17,6 +18,17 @@ const NewsBoard = () => {
       <h2 className="text-center">
         Latest <span className="badge bg-danger">News</span>
       </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        )
+      })}
     </div>
   )
 }
